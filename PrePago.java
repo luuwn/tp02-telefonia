d7cf664df5193616d7c1dbe9da647e9686617261
Gabriela Nagamuta <gnagamuta03@gmail.com>
2023-05-29 20:21:38 +0000

    Add fazerChamada

--- PrePago.java
+++ PrePago.java
@@ -1,3 +1,5 @@
+import java.util.GregorianCalendar;
+
 public class PrePago extends Assinante{
 	private Recarga [] recargas;
 	private int numRecargas;
@@ -8,5 +10,23 @@ public class PrePago extends Assinante{
 		this.creditos = 0.0f;
 		this.numRecargas = 0;
 		this.recargas = new Recarga[10];
+	}
+
+	public void fazerChamada(GregorianCalendar data, int duracao) {
+		if(numChamadas < chamadas.length) {
+			float custo = duracao * 1.45f;
+			if(creditos > custo){
+				//Chamada call = new Chamada(data, duracao, custo);
+				Chamada call = new Chamada(data, duracao);
+				chamadas[numChamadas] = call;
+				numChamadas++;
+				creditos -= custo;
+				System.out.println("CHAMADA FEITA COM SUCESSO");
+			} else {
+				System.out.println("NÃO HÁ CRÉDITOS SUFICIENTES");
+			  } 
+		} else {
+			System.out.println("O NÚMERO DE CHAMADAS MÁXIMO FOI ATINGIDO");
+		  }
 	}
 }
