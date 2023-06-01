8ff5d085b8fcca497ab4c337e9c83a54dabad442
Luan Costa <112595656+luuwn@users.noreply.github.com>
2023-06-01 01:07:33 +0000

    update telefonia - fazerchamada

--- Telefonia.java
+++ Telefonia.java
@@ -67,6 +67,65 @@ public class Telefonia{
 		}
 	}
 
+	public void fazerChamada() {
+		System.out.println("Tipo de assinante (1 - pré-pago, 2 - pós-pago): ");
+        int tipoAssinante = scan.nextInt();
+        if(tipoAssinante == 1 || tipoAssinante == 2) {
+	        System.out.println("CPF do assinante: ");
+	        long cpf = scan.nextLong();
+	
+	        if (tipoAssinante == 1) {
+	            PrePago prePago = localizarPrePago(cpf);
+	            if (prePago == null) {
+	                System.out.println("Assinante pré-pago não encontrado.");
+	                return;
+	            }
+	            System.out.println("Duração da chamada (em minutos): ");
+	            int duracao = scan.nextInt();
+	            System.out.println("Data da chamada (dd/MM): ");
+	            String d1 = scan.next();
+	            SimpleDateFormat d2 = new SimpleDateFormat("dd/MM");
+	            Date d3;
+				try {
+					d3 = d2.parse(d1);
+					GregorianCalendar data = new GregorianCalendar();
+		            data.setTime(d3);
+		            prePago.fazerChamada(data, duracao);
+		    		//System.out.println("CHAMADA FEITA COM SUCESSO");
+				} catch (ParseException e) {
+	                System.out.println("Formato de data inválido. Certifique-se de usar o formato dd/MM.");
+				}
+	            
+	            
+	            //REMOVER O CUSTO DE TODAS AS CLASSES
+	            //TENTAR GERAR O CUSTO NO MOMENTO DA CHAMADA E SEMPRE QUE FOR CHAMADO
+	        } else if (tipoAssinante == 2){
+	        	PosPago posPago = localizarPosPago(cpf);
+	            if (posPago == null) {
+	                System.out.println("Assinante pos-pago não encontrado.");
+	                return;
+	            }
+	            System.out.println("Duração da chamada (em minutos): ");
+	            int duracao = scan.nextInt();
+	            System.out.println("Data da chamada (dd/MM): ");
+	            String d1 = scan.next();
+	            SimpleDateFormat d2 = new SimpleDateFormat("dd/MM");
+	            Date d3;
+				try {
+					d3 = d2.parse(d1);
+					GregorianCalendar data = new GregorianCalendar();
+		            data.setTime(d3);
+		            posPago.fazerChamada(data, duracao);
+		    		//System.out.println("CHAMADA FEITA COM SUCESSO"); ISSO É MELHOR NA CLASSE PREPAGO E POSPAGO
+				} catch (ParseException e) {
+	                System.out.println("Formato de data inválido. Certifique-se de usar o formato dd/MM.");
+					}
+	        	}		 
+    	} else {
+        	System.out.println("DIGITE UMA OPÇÃO VÁLIDA!!!");
+        }
+    }
+
     private PrePago localizarPrePago(long cpf) {
         for (int i = 0; i < numPrePagos;i++) {
         	if (prePagos[i].getCpf() == cpf) {
