c0bbf01781a76222e9111a92a8d758d2da8fc1e7
Aline Diniz <116644964+alidiniz@users.noreply.github.com>
2023-06-02 11:28:30 +0000

    Classe PosPago

---
+++ PosPago.java
@@ -0,0 +1,12 @@
+import java.util.GregorianCalendar;
+import java.text.DecimalFormat;
+ 
+public class PosPago extends Assinante {
+ 
+	private float assinatura;
+	 
+	public PosPago(long cpf, String nome, int numero, float assinatura) {
+		super(cpf, nome, numero);
+		this.assinatura = assinatura;
+	 
+	}
