8baa57f43a48bf8612c41a62e49c0d554d845208
Aline Diniz <116644964+alidiniz@users.noreply.github.com>
2023-06-03 01:53:46 +0000

    imprimirFatura

--- PosPago.java
+++ PosPago.java
@@ -10,3 +10,21 @@ public class PosPago extends Assinante {
 		this.assinatura = assinatura;
 	 
 	}
+
+	public void imprimirFatura(int mes) {
+		double custoMensal = assinatura;
+		System.out.println("CPF: " + getCpf() + "\n" + toString());
+		for(int i = 0; i < chamadas.length; i ++) {
+			if (chamadas[i] != null) {
+				if(chamadas[i].getData().get(GregorianCalendar.MONTH) == (mes - 1)) {
+					System.out.println(chamadas[i].toString()+"| Custo: "+chamadas[i].getDuracao()*1.04f);
+					//custoMensal += chamadas[i].getCusto();
+					custoMensal += (chamadas[i].getDuracao() * 1.04f);
+				 
+				}
+			}
+		}
+		System.out.println("CUSTO DE CHAMADAS MENSAL: " + new DecimalFormat("#.##").format(custoMensal));
+	}
+ 
+}
